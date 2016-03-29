e2bf57cebfcb7614440e5cac1b58932d698209cc
barnettzy <zhuzhenyu0822@qq.com>
2016-03-30 00:03:31 +0800

    experiment update

--- experiment.py
+++ experiment.py
@@ -7,4 +7,15 @@ dataset = loaddata.load_datasets('movielens100k.data')
 datamat = model.MatrixFromData(dataset.data)
 pd = pmv.Prediction(datamat)
 
+
+file_ = open('data.dat', 'w')
+
 print datamat
+for i in range(datamat.user_count()):
+    user = datamat.preference_values_from_user(i+1)
+    for c in user[0]:
+        file_.write(str(c) + ' ')
+    file_.write('\n')
+file_.flush()
+file_.close()
+print 'done'
